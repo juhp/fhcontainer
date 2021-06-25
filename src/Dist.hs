cb0ba110823ee320b3965b3db7c6874e00d3b55e
Jens Petersen <petersen@redhat.com>
2021-06-25 22:31:35 +0800

    c8s for centos-stream

--- src/Dist.hs
+++ src/Dist.hs
@@ -9,17 +9,20 @@ import Text.ParserCombinators.ReadP (char, string)
 
 data Dist = Fedora Int -- ^ Fedora release
           | Centos Int -- ^ Centos version
+          | CentosStream Int -- ^ Centos Stream version
           | Ubi Int    -- ^ RHEL version
   deriving (Eq)
 
 instance Read Dist where
-  readPrec = choice [pFedora, pC, pCentos, pUBI] where
+  readPrec = choice [pFedora, pC, pCentos, pCS, pUBI] where
     pFedora = Fedora <$> (lift (char 'f') *> readPrec)
     pC = Centos <$> (lift (char 'c') *> readPrec)
     pCentos = Centos <$> (lift (string "centos") *> readPrec)
+    pCS = CentosStream <$> (lift (char 'c') *> readPrec <* lift (char 's'))
     pUBI = Ubi <$> (lift (string "ubi") *> readPrec)
 
 distContainer :: Dist -> String
 distContainer (Fedora n) = "fedora:" ++ show n
 distContainer (Centos n) = "centos:" ++ show n
+distContainer (CentosStream n) = "centos:stream" ++ show n
 distContainer (Ubi v) = "ubi" ++ show v
