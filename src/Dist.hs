7a9c217dc9449d6fa6850aa9c2573f1e5df2ee0d
Jens Petersen <juhpetersen@gmail.com>
2025-11-29 21:16:33 +0800

    also handle version number and eln

--- src/Dist.hs
+++ src/Dist.hs
@@ -14,9 +14,11 @@ import Numeric.Natural
 import SimpleCmd (error')
 
 data Dist = Fedora Natural -- ^ Fedora release
+          | ELN -- ^ Fedora eln
           | Centos Natural Bool Bool -- ^ Centos version devel minimal
           | Ubi Natural    -- ^ RHEL version
-          | Other String
+          | VersionNumber Natural -- ^ map to fedora or centos
+          | Other String -- ^ other distros
   deriving (Eq)
 
 parseNat :: Parser Natural
@@ -24,6 +26,9 @@ parseNat = read <$> many1 digit
 
 parseFedora :: Parser Dist
 parseFedora = Fedora <$> (char 'f' *> parseNat)
+
+parseELN :: Parser Dist
+parseELN = string "eln" $> ELN
 
 parseCentos :: Parser Dist
 parseCentos =
@@ -39,11 +44,17 @@ parseCentos =
 parseUbi :: Parser Dist
 parseUbi = Ubi <$> (string "ubi" *> parseNat)
 
+parseVersion :: Parser Dist
+parseVersion =
+  VersionNumber <$> parseNat
+
 parseOther :: Parser Dist
 parseOther = Other <$> many1 anyChar
 
+-- FIXME also handle bare numbers
 parseDist :: Parser Dist
-parseDist = try parseFedora <|> try parseCentos <|> try parseUbi <|> parseOther
+parseDist = try parseFedora <|> try parseELN <|> try parseCentos
+            <|> try parseUbi <|> parseVersion <|> parseOther
 
 readDist :: String -> Dist
 readDist s =
@@ -53,7 +64,14 @@ readDist s =
 
 distContainer :: Dist -> String
 distContainer (Fedora n) = "fedora:" ++ show n
+distContainer ELN = "registry.fedoraproject.org/eln:latest"
 -- FIXME make 11 default to development
 distContainer (Centos n d m) = "centos:stream" ++ show n ++ (mwhen d "-development") ++ (mwhen m "-minimal")
+distContainer (VersionNumber n) =
+  distContainer $
+  case compare n 11 of
+    LT -> Centos n False False
+    EQ -> ELN
+    GT -> Fedora n
 distContainer (Ubi v) = "ubi" ++ show v
 distContainer (Other s) = s
